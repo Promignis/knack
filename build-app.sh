6ec1261d3c16717ba7e64c288c97387e4935b728
raunaqrox <sahebjot94@gmail.com>
2018-08-06 04:49:27 +0530

    Add correct info.plist and resources for app to work in better resolution

--- build-app.sh
+++ build-app.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 base_dir="dist/zeno.app/Contents/MacOS"
+contents_dir="dist/zeno.app/Contents"
 mkdir -p ${base_dir}
 go build -o "${base_dir}/zeno"
 cp -r runtime "${base_dir}/."
 cp -r js "${base_dir}/."
 cp -r views "${base_dir}/."
+cp Info.plist "${contents_dir}"
+cp -r Resources ${contents_dir}
